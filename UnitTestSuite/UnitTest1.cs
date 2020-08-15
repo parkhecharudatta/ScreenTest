bad4fcd542a219895022311a3fc00db437bff7db
Charudatta Parkhe <parkhecharudatta@outlook.com>
2020-08-15 13:55:01 +0530

    Implemented UnitTest

--- UnitTestSuite/UnitTest1.cs
+++ UnitTestSuite/UnitTest1.cs
@@ -33,5 +33,65 @@ namespace UnitTestSuite
             }
         }
 
+        [TestMethod]
+        public void TestDeleteElement()
+        {
+            LinkedList linkedList = new LinkedList();
+            for (int i = 0; i < 10; i++)
+            {
+                linkedList.AddNode(new Node(i));
+            }
+
+            //Before Delete
+            string beforeDelete = linkedList.DisplayLinkedList();
+            //Delete Node 8.
+            linkedList.DeleteNode(8);
+            //After Delete
+            string afterDelete = linkedList.DisplayLinkedList();
+
+            Assert.AreNotEqual(afterDelete,beforeDelete);
+        }
+
+        [TestMethod]
+        public void TestIterationOfElements()
+        {
+            LinkedList linkedList = new LinkedList();
+            for (int i = 0; i < 10; i++)
+            {
+                linkedList.AddNode(new Node(i));
+            }
+
+            //Traversing through list.
+            for (int i = 0; i < 10; i++)
+            {
+                var node = linkedList.GetNode(i);
+                Assert.AreEqual(node.Data, i);
+            }
+        }
+
+        [TestMethod]
+        public void TestReverseIterationOfElements()
+        {
+            LinkedList linkedList = new LinkedList();
+            for (int i = 0; i < 10; i++)
+            {
+                linkedList.AddNode(new Node(i));
+            }
+
+            //Traversing through list in reverse order.
+            var currentNode = linkedList.Tail;
+
+            Assert.AreEqual(currentNode.Data,9);
+
+            var checkString = "";
+            while (currentNode != null)
+            {
+                checkString += currentNode.Data + " -> ";
+                currentNode = currentNode.PrevNode;
+            }
+            checkString += "null\r\n";
+
+            Assert.AreEqual(checkString,linkedList.DisplayReverseLinkedList());
+        }
     }
 }
